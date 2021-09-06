57cfd1d8e8c684af39ba001f4f7ec11c04e68896
Tejas7545 <76861092+Tejas7545@users.noreply.github.com>
2021-09-06 16:57:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 img="";
 Status="";
+objects=[];
 function setup(){
     canvas=createCanvas(640,420);
     canvas.center();
@@ -11,16 +12,18 @@ function preload(){
 }
 function draw(){
     image(img,0,0,640,420);
-    fill("#FF0000");
-    text("dog",45,75);
-    noFill();
-    stroke("FF0000");
-    rect(30,60,450,350);
-    fill("#FF0000");
-    text("cat",320,120);
-    noFill();
-    stroke("FF0000");
-    rect(300,90,270,320);
+    if (Status!=""){
+       for(i=0;i<objects.length;i++){
+        document.getElementById("status").innerHTML="status: object detected";
+        fill("#FF0000");
+        percent=floor(objects[i].confidence*100);
+        text(objects[i].label+" "+percent +"%",objects[i].x+15,objects[i].y+15);
+        noFill();
+        stroke("FF0000"); 
+        rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height); 
+    }
+    }
+    
 }
 function modelLoaded(){ 
     console.log("model Loaded");
@@ -32,4 +35,6 @@ function gotResult(error,results){
         console.log(error);
     }
     console.log(results);
+    objects=results;
+
 }

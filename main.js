51a026945d71eff9d973a8f5337cfb52fa45f2c3
Tejas7545 <76861092+Tejas7545@users.noreply.github.com>
2021-09-08 16:54:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,40 +1,47 @@
-img="";
-Status="";
-objects=[];
-function setup(){
-    canvas=createCanvas(640,420);
+img = "";
+Status = "";
+objects = [];
+
+function setup() {
+    canvas = createCanvas(380, 380);
     canvas.center();
-    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
-    document.getElementById("status").innerHTML="status: detecting objects";
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "status: detecting objects";
 }
-function preload(){
-    img=loadImage('dog_cat.jpg');
-}
-function draw(){
-    image(img,0,0,640,420);
-    if (Status!=""){
-       for(i=0;i<objects.length;i++){
-        document.getElementById("status").innerHTML="status: object detected";
-        fill("#FF0000");
-        percent=floor(objects[i].confidence*100);
-        text(objects[i].label+" "+percent +"%",objects[i].x+15,objects[i].y+15);
-        noFill();
-        stroke("FF0000"); 
-        rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height); 
+
+function draw() {
+    image(video, 0, 0, 380, 380);
+    if (Status != "") {
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "status: object detected";
+            document.getElementById("numberofobjects").innerHTML = "number of objects detected are: " + objects.length;
+            fill(r, g, b);
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke(r, g, b);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
     }
-    }
-    
 }
-function modelLoaded(){ 
+
+function modelLoaded() {
     console.log("model Loaded");
-    Status=true;
-    objectDetector.detect(img,gotResult);
+    Status = true;
 }
-function gotResult(error,results){
-    if (error){
+
+function gotResult(error, results) {
+    if (error) {
         console.log(error);
     }
     console.log(results);
-    objects=results;
+    objects = results;
 
 }
